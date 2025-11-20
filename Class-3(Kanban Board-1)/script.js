e0a64018f7d734d61d2eee6335ef13ae058f8687
mrinal1224 <iammrinalkc@gmail.com>
2025-11-20 12:17:50 +0530

    modal priority color

--- Class-3(Kanban Board-1)/script.js
+++ Class-3(Kanban Board-1)/script.js
@@ -4,18 +4,17 @@ let addTaskFlag = false;
 // Buttons
 const addBtn = document.querySelector(".add-btn");
 
-
 //variables
 
-let modalPriorityColor = 'lightpink'
+let modalPriorityColor = "lightpink";
 
 // Elements
 
 const modalCont = document.querySelector(".modal-cont");
 const taskArea = document.querySelector(".textArea-cont");
 const mainCont = document.querySelector(".main-cont");
-const allPriorityColors = document.querySelectorAll('.priority-color')
-console.log(allPriorityColors)
+const allPriorityColors = document.querySelectorAll(".priority-color");
+console.log(allPriorityColors);
 // Toggle Modal Open and Close
 
 addBtn.addEventListener("click", function () {
@@ -31,20 +30,33 @@ addBtn.addEventListener("click", function () {
 modalCont.addEventListener("keydown", function (e) {
   if (e.key == "Shift") {
     const task = taskArea.value;
-    generateTicket(task);
+    generateTicket(task , modalPriorityColor);
   }
 });
 
-function generateTicket(taskParam) {
+function generateTicket(taskParam, modalPriorityColor) {
   const ticketCont = document.createElement("div");
   ticketCont.setAttribute("class", "ticket-cont");
 
-  ticketCont.innerHTML = `<div class="ticket-color"></div>
+  ticketCont.innerHTML = `<div class="ticket-color" style="background-color:${modalPriorityColor}"></div>
             <div class="ticket-id">1234567</div>
             <div class="task-area">${taskParam}</div>
             <div class="ticket-lock"><i class="fa-solid fa-lock"></i></div>`;
 
   mainCont.appendChild(ticketCont);
-  modalCont.style.display = 'none'
+  modalCont.style.display = "none";
   addTaskFlag = false;
 }
+
+allPriorityColors.forEach(function (colorElem) {
+  colorElem.addEventListener("click", function () {
+    allPriorityColors.forEach(function (priorityColor) {
+      priorityColor.classList.remove("active");
+    });
+
+    colorElem.classList.add("active");
+
+    modalPriorityColor = colorElem.classList[0];
+    console.log(modalPriorityColor);
+  });
+});
