b875f6fcf890c99a151e1e4ab5926ab85492d0ad
mrinal1224 <iammrinalkc@gmail.com>
2025-11-25 13:06:01 +0530

    push json stingigy and josn parse

--- Class-3(Kanban Board-1)/test.js
+++ Class-3(Kanban Board-1)/test.js
@@ -1,28 +1,10 @@
-let movies = [
-  {
-    name: 'Inception',
-    releaseDate: "2010-07-16",
-    ratings: 8.8,
-  },
-  {
-    name: "The Dark Knight",
-    releaseDate: "2008-07-18",
-    ratings: 9.0,
-  },
-  {
-    name: "Interstellar",
-    releaseDate: "2014-11-07",
-    ratings: 8.6,
-  },
-  {
-    name: "Avatar",
-    releaseDate: "2009-12-18",
-    ratings: 7.8,
-  },
-];
-
-// let moviesJson = JSON.stringify(movies);
-// console.log(moviesJson)
 
 
-localStorage.setItem('myMovies' , JSON.stringify(movies))
+
+const movies = localStorage.getItem('myMovies')
+
+console.log(movies) // JSON
+
+const movieObjs = JSON.parse(movies)
+
+console.log(movieObjs) // JS code 
