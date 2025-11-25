006b4b2324ba76cf2a3b245db8e1125900fb7c52
mrinal1224 <iammrinalkc@gmail.com>
2025-11-25 13:07:39 +0530

    push

--- Class-3(Kanban Board-1)/test.js
+++ Class-3(Kanban Board-1)/test.js
@@ -1,9 +1,36 @@
 
+const movies = [
+    {
+      name: "Inception",
+      releaseDate: "2010-07-16",
+      ratings: 8.8
+    },
+    {
+      name: "The Dark Knight",
+      releaseDate: "2008-07-18",
+      ratings: 9.0
+    },
+    {
+      name: "Interstellar",
+      releaseDate: "2014-11-07",
+      ratings: 8.6
+    },
+    {
+      name: "Avatar",
+      releaseDate: "2009-12-18",
+      ratings: 7.8
+    }
+  ];
+  
 
 
-const movies = localStorage.getItem('myMovies')
 
-console.log(movies) // JSON
+localStorage.setItem('NewMovies' , JSON.stringify(movies))
+
+// JSON.stringify()
+const moviesFromLS = localStorage.getItem('myMovies')
+
+console.log(moviesFromLS) // JSON
 
 const movieObjs = JSON.parse(movies)
 
