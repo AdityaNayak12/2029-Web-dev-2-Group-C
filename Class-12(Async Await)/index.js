88e25fe1642b6b8011b9bd666978c30d94591b98
mrinal1224 <iammrinalkc@gmail.com>
2026-01-13 12:17:56 +0530

    callback hell

--- Class-12(Async Await)/index.js
+++ Class-12(Async Await)/index.js
@@ -1,21 +1,36 @@
 function placeOrder(drink, successCallback, errorCallback) {
-    setTimeout(() => {
-      if (drink === "coffee") {
-        successCallback("Order taken");
-      } else {
-        errorCallback("Cannot take the Order");
-      }
-    }, 1000);
-  }
-  
-  function processOrder(orderPlaced, callback) {
-    setTimeout(() => {
-      callback(`${orderPlaced} and Served.`);
-    }, 1000);
-  }
-  
-  function generateBill(callback) {
-    setTimeout(() => {
-      callback("Bill Generated");
-    }, 1000);
-  }
+  setTimeout(() => {
+    if (drink === "coffee") {
+      successCallback("Order taken");
+    } else {
+      errorCallback("Cannot take the Order");
+    }
+  }, 1000);
+} // 1
+
+function processOrder(orderPlaced, callback) {
+  setTimeout(() => {
+    callback(`${orderPlaced} and Served.`);
+  }, 1000);
+} // 2
+
+function generateBill(callback) {
+  setTimeout(() => {
+    callback("Bill Generated");
+  }, 1000);
+} // 3
+
+// execute the functions in order one after the other
+
+function serverOrder() {
+  placeOrder("coffee", function (orderPlaced) {
+    processOrder(orderPlaced, function (orderReady) {
+      generateBill(function (billgenerated) {});
+       // here
+          //here
+             //here
+    });
+  });
+}
+
+serverOrder();
