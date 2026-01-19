c188d2bd2a044a0f37d417d25766b775eb661ca4
mrinal1224 <iammrinalkc@gmail.com>
2026-01-19 16:20:15 +0530

    push

--- Class-(Promise Combinators)/combinators2.js
+++ Class-(Promise Combinators)/combinators2.js
@@ -8,22 +8,33 @@ function fetchFromOpenWeather() {
 
 function fetchFromWeatherAPI() {
   return new Promise((resolve, reject) =>
-    setTimeout(() => resolve("20 C in Bengaluru"), 700)
+    setTimeout(() => reject("No Data"), 700)
   );
 }
 
 function fetchFromAccuWeather() {
   return new Promise((resolve, reject) =>
-    setTimeout(() => resolve("20.5 C in Bengaluru"), 1200)
+    setTimeout(() => reject("No Data"), 1200)
   );
 }
 
 //
 
-Promise.race([
-  fetchFromOpenWeather(),
-  fetchFromWeatherAPI(),
-  fetchFromAccuWeather(),
+// Promise.race([
+//   fetchFromOpenWeather(),
+//   fetchFromWeatherAPI(),
+//   fetchFromAccuWeather(),
+// ]).then(function (result) {
+//   console.log(result);
+// });
+
+
+Promise.any([
+    fetchFromOpenWeather(),
+    fetchFromWeatherAPI(),
+    fetchFromAccuWeather(),
 ]).then(function (result) {
-  console.log(result);
-});
+    console.log(result);
+  }).catch(function(err){
+    console.log(err)
+  });
